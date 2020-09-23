a151eb5723937d51d785962ab3feb18bdbdb1a66
Екатерина <katia.bulygina@yandex.ru>
2020-09-23 19:51:16 +0300

    Добавляет Dto объекты для работы с продуктами

---
+++ WebStore.Domain/Dto/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Dto
+{
+    public class ProductDto:INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public BrandDto Brand { get; set; }
+        public SectionDto Section { get; set; }
+    }
+}
