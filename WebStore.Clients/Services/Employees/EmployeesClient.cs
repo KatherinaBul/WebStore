f94a1ce06f585b05b6eb561a0c8f9c63e79c6b55
Екатерина <katia.bulygina@yandex.ru>
2020-09-23 19:28:37 +0300

    Клиент для WebApi управления сотрудниками

---
+++ WebStore.Clients/Services/Employees/EmployeesClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Services.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration configuration) : base(configuration, "api/employees")
+        {
+        }
+
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(ServiceAddress);
+
+        public Employee GetById(int id) => Get<Employee>($"{ServiceAddress}/{id}");
+
+        public int Add(Employee employee) => Post(ServiceAddress, employee).Content.ReadAsAsync<int>().Result;
+
+        public void Edit(Employee employee) => Put(ServiceAddress, employee);
+
+        public bool Delete(int id) => Delete($"{ServiceAddress}/{id}").IsSuccessStatusCode;
+
+        public void SaveChanges()
+        {
+        }
+    }
+}
