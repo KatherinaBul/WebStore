4beed9f433d3341b5cbedbc609c88b1566b3ad34
Екатерина <katia.bulygina@yandex.ru>
2020-09-23 19:58:04 +0300

    Контроллер WebApi для управления товарами

---
+++ WebStore.ServicesHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServicesHosting.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : Controller, IProductData
+    {
+        private readonly IProductData _productData;
+
+        public ProductsApiController(IProductData productData) => _productData = productData;
+
+        [HttpGet("sections")]
+        public IEnumerable<Section> GetSections() => _productData.GetSections();
+
+        [HttpGet("brands")]
+        public IEnumerable<Brand> GetBrands() => _productData.GetBrands();
+
+        [HttpPost("products")]
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null) => _productData.GetProducts(filter);
+
+        [HttpGet("products/{id:int}")]
+        public Product GetProductById(int id) => _productData.GetProductById(id);
+    }
+}
