7509e3c62752b6a9ca4247d2130d40975fb64fb7
Екатерина <katia.bulygina@yandex.ru>
2020-09-27 11:18:39 +0300

    Dto объекты для WebApi системы Identity

---
+++ WebStore.Domain/Dto/Identity/ClaimDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.Dto.Identity
+{
+    public abstract class ClaimDto : UserDto
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+
+    public class AddClaimDto : ClaimDto { }
+
+    public class RemoveClaimDto : ClaimDto { }
+
+    public class ReplaceClaimDto : UserDto
+    {
+        public Claim Claim { get; set; }
+
+        public Claim NewClaim { get; set; }
+    }
+}
---
+++ WebStore.Domain/Dto/Identity/UserDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.Dto.Identity
+{
+    public abstract class UserDto
+    {
+        public User User { get; set; }
+    }
+    
+    public class AddLoginDto : UserDto
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    public class PasswordHashDto : UserDto
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDto : UserDto
+    {
+        public DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
