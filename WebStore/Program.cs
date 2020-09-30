91be55a642bbf819ed2cc9e41b0a69ecc49423de
Екатерина <katia.bulygina@yandex.ru>
2020-09-30 20:24:13 +0300

    Подлючение и конфигурирование библиотеки serilog

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace WebStore
 {
@@ -15,6 +19,17 @@ namespace WebStore
                     .ConfigureLogging((host, log) =>
                     {
                         log.AddFilter("Microsoft", level => level > LogLevel.Warning);
-                    }));
+                    }))
+                .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(
+                        outputTemplate:
+                        "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                    .WriteTo.RollingFile($@".\Log\WebStore[{DateTime.Now:yyyy-mm-ddTHH-mm-ss}].log")
+                    .WriteTo.File(new JsonFormatter(",", true),
+                        $@".\Log\WebStore[{DateTime.Now:yyyy-mm-ddTHH-mm-ss}].log.json")
+                );
     }
 }
