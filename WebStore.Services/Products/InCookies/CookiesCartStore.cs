ca2c7c4f7f4c29ffe5fd5ea18cab0dd41e987432
Екатерина <katia.bulygina@yandex.ru>
2020-10-04 16:16:52 +0300

    Сервис хранилища данных корзины

---
+++ WebStore.Services/Products/InCookies/CookiesCartStore.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Services.Products.InCookies
+{
+    public class CookiesCartStore : ICartStore
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _cartName;
+
+        public Cart Cart
+        {
+            get
+            {
+                var context = _httpContextAccessor.HttpContext;
+                var cookies = context.Response.Cookies;
+                var cartCookies = context.Request.Cookies[_cartName];
+                if (cartCookies is null)
+                {
+                    var cart = new Cart();
+                    cookies.Append(_cartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+
+                ReplaceCookies(cookies, cartCookies);
+                return JsonConvert.DeserializeObject<Cart>(cartCookies);
+            }
+            set => ReplaceCookies(_httpContextAccessor.HttpContext.Response.Cookies,
+                JsonConvert.SerializeObject(value));
+        }
+
+        private void ReplaceCookies(IResponseCookies cookies, string cookie)
+        {
+            cookies.Delete(_cartName);
+            cookies.Append(_cartName, cookie);
+        }
+
+        public CookiesCartStore(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+
+            var user = httpContextAccessor.HttpContext.User;
+            var userName = user.Identity.IsAuthenticated ? $"[{user.Identity.Name}]" : null;
+            _cartName = $"WebStore.Cart{userName}";
+        }
+    }
+}
