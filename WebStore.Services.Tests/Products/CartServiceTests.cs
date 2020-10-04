1049d120ae2831efaf55f64968019b783489921e
Екатерина <katia.bulygina@yandex.ru>
2020-10-04 16:05:23 +0300

    Новый проект для тестирования сервиса корзины. Тестирование моделей

---
+++ WebStore.Services.Tests/Products/CartServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Products
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        [TestMethod]
+        public void Cart_Class_ItemsCount_returns_Correct_Quantity()
+        {
+            var cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new CartItem { ProductId = 1, Quantity = 1 },
+                    new CartItem { ProductId = 2, Quantity = 3 },
+                }
+            };
+
+            const int expectedCount = 4;
+
+            var actualCount = cart.ItemsCount;
+
+            Assert.Equal(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cartViewModel = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            const int expectedCount = 4;
+
+            var actualCount = cartViewModel.ItemsCount;
+
+            Assert.Equal(expectedCount, actualCount);
+        }
+
+    }
+}
